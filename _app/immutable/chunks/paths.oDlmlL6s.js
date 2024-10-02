9430c0c8fae344d7fc238590469301642aefcecf
yavuzakyuz <yavuz@yavuzonair.local>
2024-10-02 03:51:18 +0200

    Updates

--- _app/immutable/chunks/paths.oDlmlL6s.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1c2o2ta)==null?void 0:s.base)??"/leaforia";var a;const e=((a=globalThis.__sveltekit_1c2o2ta)==null?void 0:a.assets)??t;export{e as a,t as b};
--- _app/immutable/entry/start.C3b2Ku14.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.CNE1GrcO.js";export{t as start};
